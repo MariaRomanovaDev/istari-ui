d343d3b05063dd6e37647811aad8db323913513d
Maria Romanova <maria.romanova@valamis.com>
2022-01-30 04:09:43 +0300

    Add dummy navigation bar

--- src/views/Comparing.tsx
+++ src/views/Comparing.tsx
@@ -2,12 +2,58 @@ import React from 'react';
 import StakingStrategies from './staking-strategies/StakingStrategies';
 import { FirstStep, Step } from './components'
 import ChoosingStrategies from './strategies/ChoosingStrategies';
+import styled from 'styled-components';
 
+const Header = styled.header`
+  height: 100px;
+  margin-bottom: 120px;
+  
+  :after, :before {
+    position: absolute;
+    content: '';
+    box-sizing: border-box;
+    background-color: black;
+  }
+  
+  :before {
+    top: 10px;
+    left: 0;
+    width: 83%;
+    height: 144px;
+    transform: skew(0deg, 3deg);
+  }
+  
+  :after {
+    top: 15px;
+    right: 0;
+    width: 17%;
+    height: 136px;
+    transform: skew(0deg, 345deg);
+  }
+  
+  nav {
+    height: 100%;
+    position: relative;
+    background-color: black;
+    display: flex;
+    justify-content: center;
+    box-sizing: border-box;
+    padding-top: 20px;
+    
+    a, img {
+      height: inherit;
+    }
+  }
+`;
 
 const Comparing: React.FC = () => {
   return (
     <>
-      <div style={{height: "100px"}} />
+      <Header>
+        <nav>
+          <a href="/"><img alt="Company's logo" src="https://staging.entity.global/static/media/logo.177bbda6.png" /></a>
+        </nav>
+      </Header>
       <FirstStep>
         <ChoosingStrategies />
       </FirstStep>
